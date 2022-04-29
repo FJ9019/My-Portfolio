0b98beb65db15657aca0671b6c787e7447465503
FJ9019 <fenelus24@gmail.com>
2022-04-29 14:12:18 -0400

    Main Script

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-(function ($) {
-    "use strict"
+var typewriter = $('.typewriter');
 
+  if(typewriter.length) {
 
-    //Navbar on scrolling
+    function initTypewriter() {
 
-$(window).scroll(function (){
-    if ($(this).scrollTop() > 200){
-        $('.navbar').fadeIn('slow').css('display','flex');
-    } else {
-        $('.navbar').fadeOut('slow').css('display', 'none');
-    }
-});
-
-    //Typed Initiate
-    if ($('.typed-text-output').length == 1){
-        var typed_strings = $('.typed-text').text();
-        var typed = new typed('.typed-text-output', {
-            strings:typed_strings.split(', '),
-            typeSpeed:100,
-            backSpeed: 20,
-            smartBackspace:false,
-            loop: true
+     var typed = new Typed(".typewriter", {
+        strings: $(".typewriter").attr("data-typewriter").split("|").map(function(e) {
+          return e
+        }),
+        typeSpeed: 80,
+        backSpeed: 75,
+        startDelay: 1000,
+        backDelay: 2000,
+        loop: !0,
+        loopcount: false,
+        showCursor: false,
+        callback: function(e){ } // call function after typing is done
         });
-    }
-})(jQuery);
+    };
+
+    initTypewriter();
+
+  };
