d5224797adc46a248959a8124b39b735eda295f4
FJ9019 <fenelus24@gmail.com>
2022-04-30 00:54:04 -0400

    Skills

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var typewriter = $('.typewriter');
 
     function initTypewriter() {
 
-     var typed = new Typed(".typewriter", {
+     var typed = new typed(".typewriter", {
         strings: $(".typewriter").attr("data-typewriter").split("|").map(function(e) {
           return e
         }),
@@ -24,6 +24,34 @@ var typewriter = $('.typewriter');
     initTypewriter();
 
   };
+
+
+  // Smooth scrolling on the navbar links
+$(".navbar-nav a").on('click', function (event) {
+   if (this.hash !== "") {
+     event.preventDefault();
+
+     $('html, body').animate({
+       scrollTop: $(this.hash).offset().top - 45
+     }, 1500, 'easeInOutExpo');
+
+     if ($(this).parents('.navbar-nav').length) {
+       $('.navbar-nav .active').remoteClass('active');
+       $(this).closest('a').addClass('active');
+     }
+   }
+});
+
+
+// skill bar
+
+$('.skill').waypoint(function (){
+  $('.progress .progress-bar').each(function (){
+    $(this).css("width", $(this).attr("aria-valuenow") + '%');
+  });
+}, {offset: '80%'});
+
+
 
 
   //Navbar on scrolling
