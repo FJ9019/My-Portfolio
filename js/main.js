6964715b105b1e39c26982e6a22d041656b7c09d
FJ9019 <jfenelus@jfenelus.com>
2022-06-13 15:28:22 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,83 +1,117 @@
+(function ($) {
+    "use strict";
 
-// My Skills
-
-$(".bar").each(function(){
-  $(this).find(".bar-inner").animate({
-    width: $(this).attr("data-width")
-  },2000)
-});
-
-// Animate Text
-
-var animate1 = {};
-animate1.opacityIn = [0,1];
-animate1.scaleIn = [0.2, 1];
-animate1.scaleOut = 8;
-animate1.durationIn = 900;
-animate1.durationOut = 700;
-animate1.delay = 600;
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.animate1 .letters-1',
-    opacity: animate1.opacityIn,
-    scale: animate1.scaleIn,
-    duration: animate1.durationIn
-  }).add({
-    targets: '.animate1 .letters-1',
-    opacity: 0,
-    scale: animate1.scaleOut,
-    duration: animate1.durationOut,
-    easing: "easeInExpo",
-    delay: animate1.delay
-  }).add({
-    targets: '.animate1 .letters-2',
-    opacity: animate1.opacityIn,
-    scale: animate1.scaleIn,
-    duration: animate1.durationIn
-  }).add({
-    targets: '.animate1 .letters-2',
-    opacity: 0,
-    scale: animate1.scaleOut,
-    duration: animate1.durationOut,
-    easing: "easeInExpo",
-    delay: animate1.delay
-  }).add({
-    targets: '.animate1 .letters-3',
-    opacity: animate1.opacityIn,
-    scale: animate1.scaleIn,
-    duration: animate1.durationIn
-  }).add({
-    targets: '.animate1 .letters-3',
-    opacity: 0,
-    scale: animate1.scaleOut,
-    duration: animate1.durationOut,
-    easing: "easeInExpo",
-    delay: animate1.delay
-  }).add({
-    targets: '.animate1 .letters-4',
-    opacity: animate1.opacityIn,
-    scale: animate1.scaleIn,
-    duration: animate1.durationIn
-  }).add({
-    targets: '.animate1 .letters-4',
-    opacity: 0,
-    scale: animate1.scaleOut,
-    duration: animate1.durationOut,
-    easing: "easeInExpo",
-    delay: animate1.delay
-  }).add({
-    targets: '.animate1',
-    opacity: 0,
-    duration: 500,
-    delay: 500
-  });
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
 
 
-// Contact //
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Scroll to Bottom
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
+
